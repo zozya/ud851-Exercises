93d8e4651bb60b330a051d8f1808f56fa52a538c
zozya <zozyq@web.de>
2017-11-26 00:33:08 +0100

    T04a.03-Exercise-PassingDataBetweenActivities ver 1

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -53,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                // TODO (1) Retrieve the text from the EditText and store it in a variable
+                // DONE (1) Retrieve the text from the EditText and store it in a variable
+                mNameEntry = (EditText) findViewById(R.id.et_text_entry);
 
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
@@ -73,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
                  */
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
-                // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+                // DONE(2) Use the putExtra method to put the String from the EditText in the Intent
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, mNameEntry.getText().toString());
 
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
