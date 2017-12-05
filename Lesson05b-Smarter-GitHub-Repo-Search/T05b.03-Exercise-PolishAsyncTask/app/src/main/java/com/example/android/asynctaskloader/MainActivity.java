434fcf950631115ae5f22d683e6dfdcebc97000b
zozya <zozyq@web.de>
2017-12-05 22:58:12 +0100

    T05b.03-Exercise-PolishAsyncTask ver 1

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -165,7 +165,8 @@ public class MainActivity extends AppCompatActivity implements
     public Loader<String> onCreateLoader(int id, final Bundle args) {
         return new AsyncTaskLoader<String>(this) {
 
-            // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
+            // DONE (1) Create a String member variable called mGithubJson that will store the raw JSON
+            String mGithubJson = null;
 
             @Override
             protected void onStartLoading() {
@@ -181,8 +182,11 @@ public class MainActivity extends AppCompatActivity implements
                  */
                 mLoadingIndicator.setVisibility(View.VISIBLE);
 
-                // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-                forceLoad();
+                // DONE (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
+                if(mGithubJson != null)
+                    deliverResult(mGithubJson);
+                else
+                    forceLoad();
             }
 
             @Override
@@ -207,8 +211,12 @@ public class MainActivity extends AppCompatActivity implements
                 }
             }
 
-            // TODO (3) Override deliverResult and store the data in mGithubJson
-            // TODO (4) Call super.deliverResult after storing the data
+            // DONE (3) Override deliverResult and store the data in mGithubJson
+            // DONE (4) Call super.deliverResult after storing the data
+            public void deliverResult(String data) {
+                mGithubJson = data;
+                super.deliverResult(data);
+            }
         };
     }
 
