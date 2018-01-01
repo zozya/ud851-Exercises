0e259368fcbfbb587319c2b9d8102b6fd075e4b8
zozya <zozyq@web.de>
2018-01-01 23:23:13 +0100

    T07.01-Exercise-CreateAContract ver 1

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -6,12 +6,19 @@ public class WaitlistContract {
 
 
 
-    // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
+    // DONE (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
 
-        // TODO (2) Inside create a static final members for the table name and each of the db columns
+        // DONE (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
+    public static class WaitlistEntry implements BaseColumns {
+        public static final String TABLE_NAME = "waitlist";
+        public static final String COLUMN_GUEST_NAME = "guestName";
+        public static final String COLUMN_PARTY_SIZE = "partySize";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
+
+        }
 
 }
