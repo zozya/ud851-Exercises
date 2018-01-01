b18e3787e050c482f74d8fecb350d5915b0a7179
zozya <zozyq@web.de>
2018-01-01 23:29:47 +0100

    T07.01-Exercise-CreateAContract ver 2

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -13,7 +13,7 @@ public class WaitlistContract {
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
-    public static class WaitlistEntry implements BaseColumns {
+    public static final class WaitlistEntry implements BaseColumns {
         public static final String TABLE_NAME = "waitlist";
         public static final String COLUMN_GUEST_NAME = "guestName";
         public static final String COLUMN_PARTY_SIZE = "partySize";
