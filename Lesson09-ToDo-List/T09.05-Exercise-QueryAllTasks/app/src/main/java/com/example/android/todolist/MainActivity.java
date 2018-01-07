a9e9020f130431f2179fce623f778794ae13c880
zozya <zozyq@web.de>
2018-01-07 22:32:28 +0100

    T09.05-Exercise-QueryAllTasks ver 1

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -29,6 +29,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
+import com.example.android.todolist.data.TaskContract;
+
 
 public class MainActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
@@ -146,10 +148,23 @@ public class MainActivity extends AppCompatActivity implements
             public Cursor loadInBackground() {
                 // Will implement to load data
 
-                // TODO (5) Query and load all task data in the background; sort by priority
+                // DONE (5) Query and load all task data in the background; sort by priority
                 // [Hint] use a try/catch block to catch any errors in loading data
+                try {
 
-                return null;
+                    return getContentResolver().query(TaskContract.TaskEntry.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            TaskContract.TaskEntry.COLUMN_PRIORITY);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                    return null;
+                }
+
+                
             }
 
             // deliverResult sends the result of the load, a Cursor, to the registered listener
--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,37 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // DONE (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        // DONE (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
+        Cursor cursor = null;
+        switch(match) {
 
-        // TODO (3) Query for the tasks directory and write a default case
+            case TASKS:
+                cursor = db.query(TaskContract.TaskEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
 
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
+            default:
+                throw new UnsupportedOperationException("Uri is unknown: " + uri);
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        }
+
+        // DONE (3) Query for the tasks directory and write a default case
+
+        // DONE (4) Set a notification URI on the Cursor and return that Cursor
+
+    if(cursor != null)
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
 
