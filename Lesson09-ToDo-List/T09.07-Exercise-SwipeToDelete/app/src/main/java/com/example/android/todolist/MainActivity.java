10976427fd6894b6a72c35684bc4596a817e42fa
zozya <zozyq@web.de>
2018-01-08 13:34:13 +0100

    T09.07-Exercise-SwipeoDelete ver 1

--- Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.todolist;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
@@ -78,12 +79,19 @@ public class MainActivity extends AppCompatActivity implements
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 // Here is where you'll implement swipe to delete
 
-                // TODO (1) Construct the URI for the item to delete
+                // DONE (1) Construct the URI for the item to delete
                 //[Hint] Use getTag (from the adapter code) to get the id of the swiped item
+                int id = (int) viewHolder.itemView.getTag();
+                String sId = Integer.toString(id);
 
-                // TODO (2) Delete a single row of data using a ContentResolver
+                Uri uri = TaskContract.TaskEntry.CONTENT_URI;
+                uri = uri.buildUpon().appendPath(sId).build();
 
-                // TODO (3) Restart the loader to re-query for all tasks after a deletion
+                // DONE (2) Delete a single row of data using a ContentResolver
+                getBaseContext().getContentResolver().delete(uri, null, null);
+
+                // DONE (3) Restart the loader to re-query for all tasks after a deletion
+                getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, MainActivity.this);
                 
             }
         }).attachToRecyclerView(mRecyclerView);
